462265c8962e29269c0648957b17d68c67a76bb5
Aeneid201 <aloubi.oumaima@gmail.com>
2024-12-08 13:25:43 -0500

    setup pool connection string for production

--- db/pool.js
+++ db/pool.js
@@ -1,18 +1,19 @@
 const { Pool } = require("pg");
 require('dotenv').config({path: './config/.env'})
 
-const pool = new Pool({
-  host: process.env.HOSTNAME,
-  user: process.env.USER,
-  database: "postgres",
-  password: process.env.ROLE_PASSWORD,
-  port: 5432
-});
+// const pool = new Pool({
+//   host: process.env.HOSTNAME,
+//   user: process.env.USER,
+//   database: "postgres",
+//   password: process.env.ROLE_PASSWORD,
+//   port: 5432
+// });
 
-//   const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: { rejectUnauthorized: false },
-//   });
+// production
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 
 module.exports = pool;
